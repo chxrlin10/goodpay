04eb1a9fcdbbe1aa4c01e7e805a3ff1e4acf347d
Charles <charlesrocha1001@gmail.com>
2022-09-30 13:42:47 -0300

    Modal

---
+++ js/moduloDados.js
@@ -0,0 +1,33 @@
+/* dentro deste array existem usuarios com esta estrutura:
+-id
+nomeUsuario
+senha
+email
+*/
+
+const usuarios= [
+    {
+        id: 1,
+        nomeUsuario: "charles-cauan",
+        senha: "12345",
+        email: "charlesrocha1001@gmail.com",
+    },
+
+    {
+        id: 2,
+        nomeUsuario: "Cauan-rocha",
+        senha: "56789",
+        email: "cauan-rocha@hotmail.com",
+    },
+
+    {
+        id: 3,
+        nomeUsuario: "Cauan Raimundo",
+        senha: "54321",
+        email:"raimundo@hotmail.com",
+    }
+
+]
+
+export default usuarios;
+/* disponibiliza o array para ser utilizado  fora do arquivo moduloDados */
